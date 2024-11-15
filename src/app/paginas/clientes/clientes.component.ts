dbb8d549e28c22b36e6792eb8b35833406b1b24e
Claudionilton <claudioniltonjose@gmail.com>
2024-11-15 06:25:13 -0300

    consumindo api

--- src/app/paginas/clientes/clientes.component.ts
+++ src/app/paginas/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ICliente } from '../../interfaces/icliente';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-clientes',
@@ -12,12 +13,39 @@ import { ICliente } from '../../interfaces/icliente';
 export class ClientesComponent implements OnInit{
 
   public clientes: Array<ICliente> = []
+  #http = inject(HttpClient)
 
   ngOnInit(): void {
-    this.clientes.push({id: 1, name: {firstname: 'Claudionilton',lastname:'Júnior'}, email: 'claudionilton.junior@gmail.com'})
+    this.clientes = this.getClientes()
+  /*  this.clientes.push({id: 1, name: {firstname: 'Claudionilton',lastname:'Júnior'}, email: 'claudionilton.junior@gmail.com'})
     this.clientes.push({id: 2, name: {firstname: 'Jose',lastname:'Nascimento'}, email: 'jose.nascimento@gmail.com'})
     this.clientes.push({id: 3, name: {firstname: 'Paula',lastname:'Gabriela'}, email: 'paula.gabriela@gmail.com'})
     this.clientes.push({id: 4, name: {firstname: 'Saulo',lastname:'Gabriel'}, email: 'saulo.gabriel@gmail.com'})
-  }
+  */
+    }
+  private getClientes(): Array<ICliente> {
 
+    let clientes: ICliente[] = []
+    let url: string = 'https://fakestoreapi.com/users'
+    let req = this.#http.get<Array<ICliente>>(url)
+    let sub = {
+      next(dados: Array<ICliente>) {
+        console.log(`next`,dados)
+        dados.forEach((el: ICliente) => {
+          clientes.push({id: el.id, name: el.name, email: el.email})
+        })
+        console.log('clientes :>> ', clientes);
+      },
+      error(err: any) {
+        console.log(`error`, err)
+      },
+      complete() {
+        console.log(`Complete`)
+      }
+    }
+
+    req.subscribe(sub)
+
+    return clientes
+  }
 }
